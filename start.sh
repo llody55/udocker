95253f0393a35fb394accfae13f0eaa66ffb70a9
llody_55 <745719408@qq.com>
2024-06-26 11:50:20 +0800

    新增arm64

--- start.sh
+++ start.sh
@@ -5,8 +5,10 @@ ARCH=$(uname -m)
 
 # 启动udocker程序，并让其在后台运行
 if [ "$ARCH" = "x86_64" ]; then
+  chmod +x ./bin/amd64/udocker
   ./bin/amd64/udocker &
 elif [ "$ARCH" = "aarch64" ]; then
+  chmod +x ./bin/arm64/udocker
   ./bin/arm64/udocker &
 else
   echo "Unsupported architecture"
